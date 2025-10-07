dcff9317a2a78df7046ca6a9f87b2109d59dfcea
Ingrid Mendes <dinha@MacBook-Pro-de-Ingrid.local>
2025-10-06 23:47:14 -0300

    resource service secure by jwt

---
+++ secured-service/src/main/java/br/com/ingridmendes/secured_service/SecuredServiceApplication.java
@@ -0,0 +1,30 @@
+package br.com.ingridmendes.secured_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SecuredServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredServiceApplication.class, args);
+	}
+
+	@RestController
+	class ResourceController {
+		@GetMapping("/resource")
+		public String getResource(@AuthenticationPrincipal Jwt jwt) {
+			var strBuilder = new StringBuilder();
+			strBuilder.append("<h1> Secured Resource </h1>")
+					.append("<p> JWT Token %s</p>".formatted(jwt.getTokenValue()))
+					.append("<p> JWT Headers %s</p>".formatted(jwt.getHeaders()))
+					.append("<p> JWT Headers %s</p>".formatted(jwt.getClaims().toString()))
+					.append("<p> Resource accessed by: %s (with subjsct: %s)</p>".formatted(jwt.getAudience(), jwt.getSubject()));
+			return strBuilder.toString();
+		}
+	}
+}
